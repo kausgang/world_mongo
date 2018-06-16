47ec2b10ae0979c5c6596d2ded88dbd20d06d974
Kaustav Ganguli <write.to.kaustav@gmail.com>
2018-06-16 00:58:07 -0400

    included personality svg

--- public/javascripts/show_personality.js
+++ public/javascripts/show_personality.js
@@ -1,18 +1,34 @@
 function show_personality(year,country_name){
 
+    
     var width = 860,
-        height = 600;
+    height = 500;
 
     var svg = d3.select('#personality')
-                .attr("width", width)
-                .attr("height", height);
-
-
+        .attr("width", width)
+        .attr("height", height);
+        
     // CLEAR OUT THE PREVIOUS CONTENT FIRST
     // https://stackoverflow.com/questions/10784018/how-can-i-remove-or-replace-svg-content
     svg.selectAll("*").remove();
 
+
+
+
+    
+
+
+
+
+
+    
     d3.csv('/PERSONALITY/personality.csv',function(d){
+
+
+
+
+  
+
 
         // FORMAT THE INPUT TO SELECT ONLY THE LIVING PERSONALITIES 
         // REMOVE ALL THE DATA THAT IS NOT LIVING IN CURRENT YEAR
@@ -64,7 +80,7 @@ function show_personality(year,country_name){
         })
  
 
-        
+
 
 
         /* svg.selectAll('.picture')
@@ -155,8 +171,133 @@ function show_personality(year,country_name){
              });
 
  */
-            
+
+
+// var tip = d3.tip()
+// .attr('class', 'd3-tip')
+// .offset([-10, 0])
+// .html(function(d) {
+//     var age = (year - parseInt(d.born));
+//     return "<strong>Age:</strong> <span style='color:red'>" + age + "</span>";
+// })
+
+
       
+        var cx = 50;
+        var circle_radius = 40;
+        var age_thickness = 15;
+        
+
+        
+        svg.selectAll("g")
+            .data(formatted_array2)
+            .enter()
+            .append("defs")
+                .append("pattern")
+                    .attr("id",function(d,i){
+                        return "attachedImage"+i;
+                    })
+                    .attr("height","100%")
+                    .attr("width","100%")
+                    .attr("patternContentUnits","objectBoundingBox")
+                .append("image")
+                    .attr("xlink:href",function(d){
+
+                        var geography = d.geography;
+                        if(geography == country_name){
+                            return d.picture;
+                        }
+                        
+                    })
+                    .attr("width","1")
+                    .attr("height","1")
+                    .attr("class","person_image")
+
+
+        svg.selectAll("g")
+            .data(formatted_array2)
+            .enter()
+            .append("circle")
+                .attr("cx",cx)
+                .attr("cy",function(d,i){
+
+                    var cy = 2*circle_radius*i + 2*circle_radius;
+                    return cy;
+                })
+                .attr("r",circle_radius)
+                .attr("fill",function(d,i){
+                    return "url(#attachedImage"+i;
+                })
+                .attr("class","person_circle")
+        
+        svg.selectAll("g")
+            .data(formatted_array2)
+            .enter()
+            .append("rect")
+                .attr("x",function(d,i){
+                    var rect_x = cx+circle_radius+10;
+                    return rect_x;
+                })
+                .attr("y",function(d,i){
+
+                    var cy = 2*circle_radius*i + 2*circle_radius;
+                    var rect_y = cy - age_thickness/2;
+                    return rect_y;
+                })
+                .attr("width",function(d){
+
+                    var geography = d.geography;
+                    if(geography == country_name){
+                        var age = (year - parseInt(d.born))*7;
+                        return age; //MULTIPLYING WITH 7 SO THAT THE WIDTH TAKES UP SOME SPACE IN SVG
+                    }
+                })
+                .attr("height",age_thickness)
+                .attr("class","person_age")
+                // .on('mouseover', tip.show)
+                // .on('mouseout', tip.hide)
+
+                
+
+
+        
+
+        svg.selectAll("g")
+            .data(formatted_array2)
+            .enter()
+            .append("text")
+                .attr("x",function(d,i){
+                    var rect_x = cx + circle_radius+10;
+                    var age = (year - parseInt(d.born))*7;
+                    var text_x = rect_x + age + 10 ;
+
+                    var geography = d.geography;
+                    if(geography == country_name){
+                        return text_x;
+                    }
+                    
+                })
+                .attr("y",function(d,i){
+                   
+                    var cy = 2*circle_radius*i + 2*circle_radius;
+                    var rect_y = cy - age_thickness/2;
+                    var text_y = rect_y +  age_thickness/2 ;
+
+                    var geography = d.geography;
+                    if(geography == country_name){
+                        return text_y;
+                    }
+                })
+                .text(function(d){
+                    var geography = d.geography;
+                    if(geography == country_name){
+                        return (year - parseInt(d.born)) 
+                    }
+                    else    
+                        return '';
+
+                })
+                .attr("class","person_age_number")
 
 
 
